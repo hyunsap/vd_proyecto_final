7022c12be5e973967cf5134673cf98fc5ac7ad07
hyunsap <marianazunino2003@gmail.com>
2024-06-06 16:41:17 -0300

    pcs

--- src/App.svelte
+++ src/App.svelte
@@ -1,183 +1,112 @@
 <script>
-  import Scroller from "@sveltejs/svelte-scroller"
-  import {onMount} from "svelte"
-  import * as d3 from "d3"
+    import Scroller from "@sveltejs/svelte-scroller"
+    import {onMount} from "svelte"
+    import * as d3 from "d3"
+    import DebugScroller from "./components/DebugScroller.svelte"
+    import Loremipsum from "./components/Loremipsum.svelte"
 
-  import Medallero from "./components/Medallero.svelte"
-  import DebugScroller from "./components/DebugScroller.svelte"
-  import Loremipsum from "./components/Loremipsum.svelte"
+    /* Variables para el scroller1 */
+    let count
+    let index
+    let offset
+    let progress
+    let top = 0.1
+    let threshold = 0.5
+    let bottom = 0.9
 
-  /* Variables para la data del medallero */
-  let deportistas = []
-  let filteredDeportistas = []
+    /* Variables para el scroller 2 */
+    let count2
+    let index2
+    let offset2
+    let progress2
+    let top2 = 0.1
+    let threshold2 = 0.5
+    let bottom2 = 0.9
 
-  /* Variables para el scroller1 */
-  let count
-  let index
-  let offset
-  let progress
-  let top = 0.1
-  let threshold = 0.5
-  let bottom = 0.9
+    /*Imagen cambiante pcs*/
+    let pc_colec = ["images/pcs0.png", "images/pcs1.png", "images/pcs2.png", "images/pcs3.png", "images/pcs4.png", "images/pcs5.png"]
+    let i = 0
 
-  /* Variables para el scroller 2 */
-  let count2
-  let index2
-  let offset2
-  let progress2
-  let top2 = 0.1
-  let threshold2 = 0.5
-  let bottom2 = 0.9
-
-  /* Charts */
-  let charts = {
-    0: "lines_01.png",
-    1: "lines_02.png",
-    2: "lines_03.png",
-  }
-
-  onMount(() => {
-    d3.csv("./data/deportistas.csv", d3.autoType).then(data => {
-      deportistas = data
-      filteredDeportistas = deportistas
-    })
-  })
-
-  $: {
-    // Es un observer que se ejecuta cuando cambia el valor de index
-    switch (index) {
-      case 0:
-        filteredDeportistas = deportistas
-        break
-      case 1:
-        filteredDeportistas = deportistas.filter(d => d.genero === "F")
-        break
-      case 2:
-        filteredDeportistas = deportistas.filter(d => d.genero === "M")
-        break
-      case 3:
-        filteredDeportistas = deportistas.filter(
-          d => d.continente === "América",
-        )
-        break
-      default:
-        filteredDeportistas = deportistas
+    /* Charts */
+    let charts_ventas = {
+        0: "albumes_ventas_nopcs.png",
+        1: "albumes_ventas_pcs.png",
+        2: "albumes_ventas_ww_sk.png",
     }
-    console.log(filteredDeportistas)
-  }
+
+    let chart_tendencia_ventas = "tendencia_ventas_punt_pcs.png"
+    let chart_ganancia_ventas = "ganancia_ventas.png"
+
+    function handleButtonClick() {
+        const textoAnimado = document.createElement("div");
+        textoAnimado.className = "animated-text";
+        textoAnimado.textContent = "-$30";
+
+        const contenedor = document.getElementById("contenedor");
+        contenedor.appendChild(textoAnimado);
+
+        // Eliminar el texto animado después de la animación
+        setTimeout(() => {
+            contenedor.removeChild(textoAnimado);
+        }, 1000); // Debe coincidir con la duración de la animación en CSS
+    }
+
+    function handleButtonClickWithPhotocard() {
+        const textoAnimado = document.createElement("div");
+        textoAnimado.className = "animated-text";
+        textoAnimado.textContent = "-$30 + 1 photocard";
+
+        i=i+1;
+
+        const contenedor = document.getElementById("contenedor2");
+        contenedor.appendChild(textoAnimado);
+
+        // Eliminar el texto animado después de la animación
+        setTimeout(() => {
+            contenedor.removeChild(textoAnimado);
+        }, 1000); // Debe coincidir con la duración de la animación en CSS
+    }
+
+    onMount(()=>{})
 </script>
 
 <main>
-  <div class="header">
-    <img src="/images/olympics-logo.png" width="100" alt="anillos" />
-    <h3 class="headline">
-      <b>Triunfos Olímpicos</b>
-      Medallas, alturas y continentes
-    </h3>
-    <p class="bajada">Explorando los logros olímpicos a través de datos</p>
-    <div class="lorem_ipsum">
-      <Loremipsum />
+    <div class="header">
+      <img src="/images/Group 2.png" width="200" alt="pcs_template" />
+      <h3 class="headline">
+        <b>El nuevo gigante de la industria musical: </b>
+        kpop supera en ventas físicas al resto de los géneros
+      </h3>
+      <p class="bajada">Explorando el efecto del consumismo fan en las ventas de álbumes</p>
+      <div class="lorem_ipsum">
+        <Loremipsum />
+      </div>
     </div>
-  </div>
 
-  {#if progress < 1}
-  <DebugScroller
-    index={index}
-    count={count}
-    offset={offset}
-    progress={progress}
-  />
-  {/if}
-  <!-- Primer scroller -->
-  <Scroller
-    top={top}
-    threshold={threshold}
-    bottom={bottom}
-    bind:count={count}
-    bind:index={index}
-    bind:offset={offset}
-    bind:progress={progress}
-  >
-    <div slot="background">
-      <Medallero deportistas={filteredDeportistas} />
+    <div class="gamification_portion">
+        <div class="album_no_pcs">
+            <p>Compra tantos álbumes como quieras</p>
+            <img src="/images/1989_taylor.png" width=400 alt="ts">
+            <button on:click={handleButtonClick}>Comprar</button>
+            <div id="contenedor"></div>
+        </div>
+        <div class="album_pcs">
+            <p>Nuevamente, compra tantos álbumes como quieras</p>
+            <div class="cero_pcs" style="display:flex; align-items: center; column-gap: 250px">
+                <img src="/images/newjeans.png" width=300 alt="nwjns">
+                <img id="img_pc" src={pc_colec[i]} width=500 alt="nopcs">
+            </div>
+            <button on:click={handleButtonClickWithPhotocard}>Comprar</button>
+            <div id="contenedor2"></div>
+           
+        </div>
     </div>
-    <div slot="foreground" class="foreground_container">
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Todos los deportistas</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas femeninas</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas masculinos</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index + 1}</h3>
-          <p>Deportistas americanos</p>
-        </div>
-      </section>
-    </div>
-  </Scroller>
 
-  <div class="lorem_ipsum">
-    <Loremipsum />
-  </div>
-  
 
-  <!-- Segundo scroller -->
-  <Scroller
-    top={top2}
-    threshold={threshold2}
-    bottom={bottom2}
-    bind:count={count2}
-    bind:index={index2}
-    bind:offset={offset2}
-    bind:progress={progress2}
-  >
-    <div slot="background" class="image_container">
-      <img src="/images/{charts[index2]}" alt="chart {index2}" class="charts"
-      />
-    </div>
-    <div slot="foreground" class="foreground_container">
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-      <section class="step_foreground">
-        <div class="epi_foreground">
-          <h3>Seccion {index2 + 1}</h3>
-          <p>Gráfico 1</p>
-        </div>
-      </section>
-    </div>
-  </Scroller>
 </main>
 
-<div class="lorem_ipsum">
-  <Loremipsum />
-</div>
-
 <style>
-
-  .header {
+    .header {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -202,13 +131,37 @@
     display: block;
   }
 
-  /* Estilos para el scroller */
+  #contenedor {
+    position: relative;
+    display: inline-block;
+}
+
+.animated-text {
+    position: absolute;
+    color: red;
+    font-size: 40px;
+    font-weight: bold;
+    animation: fadeOutMoveUp 1s forwards;
+}
+
+@keyframes fadeOutMoveUp {
+    0% {
+        opacity: 1;
+        top: 0;
+    }
+    100% {
+        opacity: 0;
+        top: -50px;
+    }
+}
+
+  /* Estilos para el scroller
   .foreground_container {
     pointer-events: none;
     padding-left: 50%;
-  }
+  } -->
 
-  .step_foreground {
+  <!-- .step_foreground {
     display: flex;
     justify-content: end;
     align-items: center;
@@ -217,12 +170,12 @@
     color: white;
     padding: 1em;
     margin: 0 0 2em 0;
-  }
-  .epi_foreground {
+  } -->
+  <!-- .epi_foreground {
     padding: 20px;
     max-width: 150px;
     background-color: rgba(0, 0, 0, 0.5);
-  }
+  } --> */
   .lorem_ipsum {
     margin: 100px auto;
     max-width: 740px;
