358f53a3b6a8bf8b4fcbd8137cc64babaa8bc50d
hyunsap <marianazunino2003@gmail.com>
2024-06-09 01:43:14 -0300

    erarararefrnheomfoes

--- src/App.svelte
+++ src/App.svelte
@@ -29,13 +29,13 @@
 
     /* Charts */
     let charts_ventas = {
-        0: "albumes_ventas_nopcs.png",
-        1: "albumes_ventas_pcs.png",
-        2: "albumes_ventas_ww_sk.png",
+        0: "/images/albumes_ventas_nopcs.png",
+        1: "/images/albumes_ventas_pcs.png",
+        2: "/images/albumes_ventas_ww_sk.png",
     }
 
-    let chart_tendencia_ventas = "tendencia_ventas_punt_pcs.png"
-    let chart_ganancia_ventas = "ganancia_ventas.png"
+    let chart_tendencia_ventas = "/images/tendencia_ventas_punt_pcs.png"
+    let chart_ganancia_ventas = "/images/ganancia_ventas.png"
 
     function handleButtonClick() {
         const textoAnimado = document.createElement("div");
@@ -118,14 +118,10 @@
         bind:offset={offset}
         bind:progress={progress}
       >
-      <div slot="ce">
-        <p> hola</p>
-      </div>
       <div slot="foreground" class="foreground_container">
         <section class="step_foreground">
           <div class="epi_foreground">
-            <!-- <h3>Seccion {index + 1}</h3> -->
-            <p>Compra un album</p>
+            <h3>Seccion {index + 1}</h3>
           </div>
         </section>
       </div>
@@ -141,29 +137,60 @@
       bind:progress={progress2}
       >
       <div slot="background" class="image_container">
-        <img src="/images/{charts_ventas[index2]}" width=600px alt="chart {index2}" class="charts"/>
+        <img src={charts_ventas[index2]} width=600px alt="chart {index2}" class="charts"/>
       </div>
       <div slot="foreground" class="foreground_container">
         <section class="step_foreground">
           <div class="epi_foreground">
-            <h3>Seccion {index2 + 1}</h3>
-            <p>Gráfico 1</p>
+            <h3>¿Cómo se ve el mercado de ventas en música?</h3>
+            <p>Las ventas de álbumes occidentales en la última década presentan una baja
+              de ventas físicas gradual, en gran parte causada por la aparición de los 
+              servicios de streaming.
+            </p>
           </div>
         </section>
         <section class="step_foreground">
           <div class="epi_foreground">
-            <h3>Seccion {index2 + 1}</h3>
-            <p>Gráfico 1</p>
+            <h3>¿Qué diferencia se encuentra en kpop?</h3>
+            <p>Por el contrario, las ventas en kpop cada año aumentan de manera exponencial,
+              ya que ofrecen una experiencia de consumidor más personalizada y llamativa.
+              Todo el diseño del álbum y sus inclusiones están pensados para la venta masiva.
+            </p>
           </div>
         </section>
         <section class="step_foreground">
           <div class="epi_foreground">
-            <h3>Seccion {index2 + 1}</h3>
-            <p>Gráfico 1</p>
+            <h3>¿Cómo se afecta al mercado actual?</h3>
+            <p>Se ha llegado a un punto en el que la venta de álbumes de kpop ha superado
+               a los álbumes del resto de la industria musical, causando una especie de 
+               renacimiento del mercado musical físico</p>
           </div>
         </section>
       </div>
     </Scroller>
+    <div class="top20">
+      <p>En el 2023 ha ocurrido un suceso histórico: 19 de los 20 álbumes mejores vendidos
+         forman parte del género musical emergente desde hace ya varios años. 
+         Según los registros de la IFPI (Federación Internacional de la Industria Fonográfica), 
+         el único álbum cuyas ventas físicas alcanzaron el top es 1989 (Taylor’s Version), 
+         lanzado por una de las artistas más populares del momento Taylor Swift. 
+      </p>
+      <p>Este fenómeno encuentra una posible explicación en una característica muy 
+        particular de los álbumes de kpop: las inclusiones. 
+        Especialmente, las photocards. Se trata de piezas coleccionables randomizadas, 
+        con fotos de artistas, muy codiciadas por los fans.</p>
+      <p>Cada álbum comprado contiene generalmente 1 o 2 de estas piezas, 
+        y la colección total puede ser muy extensa, llegando a más de 100 
+        photocards totales en algunos casos. Por ello, se ha extendido en la 
+        comunidad fan del kpop la práctica de comprar álbumes en cantidad con tal de 
+        completar una colección de photocards, y luego descartar los discos repetidos. 
+      </p>
+      <p>Se puede encontrar entonces una correlación muy fuerte entre 
+        cantidad de ventas físicas de un álbum y la cantidad total de photocards 
+        que pertenecen a la colección de dicho álbum.
+      </p>
+      <img src={chart_tendencia_ventas} alt="ventasxpcs" width=500/>
+    </div>
     </div>
 
 
@@ -200,13 +227,13 @@
     display: inline-block;
 }
 
-.animated-text {
+  .animated-text {
     position: absolute;
     color: red;
     font-size: 40px;
     font-weight: bold;
     animation: fadeOutMoveUp 1s forwards;
-}
+  }
 
 @keyframes fadeOutMoveUp {
     0% {
